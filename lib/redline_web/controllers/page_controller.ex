857b45e480de9d8b5ffc39d514e96d15d7592b18
Bill Peregoy <bill@peregoy.org>
2019-07-21 20:48:31 -0400

    Adding basic Elm functionality

---
+++ lib/redline_web/controllers/page_controller.ex
@@ -0,0 +1,7 @@
+defmodule RedlineWeb.PageController do
+  use RedlineWeb, :controller
+
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+end
--- lib/redline_web/router.ex
+++ lib/redline_web/router.ex
@@ -1,8 +1,22 @@
 defmodule RedlineWeb.Router do
   use RedlineWeb, :router
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  scope "/", RedlineWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
   end
 
   scope "/api" do
